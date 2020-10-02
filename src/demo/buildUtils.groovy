d7eb0da0e1852f56867e35e5badd2641f394bce1
Dong Ho Lee <dhlee@ee.knu.ac.kr>
2020-10-02 14:20:17 +0900

    correct

--- src/demo/buildUtils.groovy
+++ src/demo/buildUtils.groovy
@@ -4,7 +4,7 @@ package demo
 
 class buildUtils implements Serializable {
    def steps
-   buildUtils() { this.steps = 'steps'}
+   buildUtils(steps) { this.steps = steps}
    def timedGradleBuild(tasks) {
       def gradleHome = 'gradle3.2'
       echo ${gradleHome}/bin/gradle ${steps} ${tasks}
